fba3007f8fd9d91bcfb299831406becd26ac67e0
AntonenkoVV <antonenkovera05@gmail.com>
2024-07-09 22:56:35 +0300

    добавлено масштабирование в svg

--- main.cpp
+++ main.cpp
@@ -130,10 +130,28 @@ void show_histogram_svg(const vector<size_t>& bins) {
 
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
 
+    //моё масштабирование svg
+    //находим наибольший столбец
+    size_t max_count = bins[0];
+    for (auto bin : bins){
+        if (bin > max_count)
+            max_count = bin;
+    }
+
+    // Масштабирование количества звездочек
+    vector<size_t> bins_mash = bins;
+    if ((max_count * BLOCK_WIDTH) > (IMAGE_WIDTH - TEXT_WIDTH)){
+        for (size_t i = 0; i < bins.size(); i++) {
+            size_t height = MAX_ASTERISK * (static_cast<double>(bins[i]) / max_count);
+            bins_mash[i] = height;
+        }
+    }
+
+    //вывод гистограмы в svg
     double top = 0;
-    for (size_t bin : bins) {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    for (size_t i = 0; i < bins_mash.size(); i++) {
+        const double bin_width = BLOCK_WIDTH * bins_mash[i];
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bins[i]));
         svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT); //"deeppink", "#FF69B4"
         top += BIN_HEIGHT;
     }
